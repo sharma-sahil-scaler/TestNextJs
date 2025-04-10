6744a72589044115f31646ca0279045930ecebca
sharma.sahil <sharma.sahil@scaler.com>
2025-04-10 21:40:51 +0530

    fix:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import nextImg from "@/public/next.svg";
+import vercelImg from "@/public/vercel.svg";
+import fileImg from "@/public/file.svg";
+import windowImg from "@/public/window.svg";
+import globeImg from "@/public/globe.svg";
 
 export default function Home() {
   return (
@@ -7,7 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={nextImg}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +34,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src={vercelImg}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +59,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src={fileImg}
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src={windowImg}
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +87,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src={globeImg}
             alt="Globe icon"
             width={16}
             height={16}
